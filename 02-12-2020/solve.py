65b944d8d315bbe7ef8488487ecd5105040d79d6
freekdh <freekdh@gmail.com>
2020-12-02 08:49:38 -0800

    solve day 2

--- 02-12-2020/solve.py
+++ 02-12-2020/solve.py
@@ -21,7 +21,28 @@ def main():
         if low_count <= password.count(letter) <= high_count:
             valid_passwords += 1
 
-    print(f"{valid_passwords} valid passwords out of {len(input_data)} passwords")
+    print(
+        f"part1: {valid_passwords} valid passwords out of {len(input_data)} passwords"
+    )
+
+    # part 2
+
+    valid_passwords = 0
+    for database_line in input_data:
+        first_digit, second_digit, letter, password = re.match(
+            pattern="(\d+)-(\d+)\s(.):\s(.*)", string=database_line
+        ).groups()
+
+        first_digit, second_digit = int(first_digit), int(second_digit)
+
+        if (password[first_digit - 1] == letter) != (
+            password[second_digit - 1] == letter
+        ):
+            valid_passwords += 1
+
+    print(
+        f"part2: {valid_passwords} valid passwords out of {len(input_data)} passwords"
+    )
 
 
 if __name__ == "__main__":
